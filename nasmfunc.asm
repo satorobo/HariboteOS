786cd4a9121644a504f14987266656573cb8fc0c
satorobo <sato.robo.0729@gmail.com>
2022-01-18 00:28:17 +0900

    Add the function that writes an integer at a specific address

--- nasmfunc.asm
+++ nasmfunc.asm
@@ -7,3 +7,9 @@ section .text
 io_hlt:
     HLT
     RET
+
+write_mem8:             ; void write_mem8(int addr, int data)
+    MOV   ECX, [ESP+4]  ; [ESP+4] point to addr
+    MOV   AL, [ESP+8]   ; [ESP+8] point to data
+    MOV   [ECX], AL
+    RET
