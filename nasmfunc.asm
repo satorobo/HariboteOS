f66f02f428ff42a88d2c97af531b857810dc0fab
satorobo <sato.robo.0729@gmail.com>
2022-01-22 01:55:58 +0900

    Remove the function that writes an integer at a specific address

--- nasmfunc.asm
+++ nasmfunc.asm
@@ -1,15 +1,8 @@
 ; nasmfunc.asm
 bits 32
 global io_hlt
-global write_mem8
 
 section .text
 io_hlt:
     HLT
-    RET
-
-write_mem8:             ; void write_mem8(int addr, int data)
-    MOV   ECX, [ESP+4]  ; [ESP+4] point to addr
-    MOV   AL, [ESP+8]   ; [ESP+8] point to data
-    MOV   [ECX], AL
     RET
